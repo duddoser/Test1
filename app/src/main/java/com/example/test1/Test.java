82d602e75336718400ad000ba01b6ac107ea2139
ne_sanya <barinovaaalexandra@gmail.com>
2020-01-28 22:54:05 +0300

    that stuff works

--- app/src/main/java/com/example/test1/Test.java
+++ app/src/main/java/com/example/test1/Test.java
@@ -2,6 +2,7 @@ package com.example.test1;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
@@ -9,44 +10,90 @@ import android.view.SurfaceView;
 
 class Test extends SurfaceView implements SurfaceHolder.Callback{
     private SurfaceHolder surfaceHolder;
-    private Canvas canvas;
-    private int startRadius;
-
+    private DrawThread drawThread;
 
     public Test(Context context) {
         super(context);
         getHolder().addCallback(this);
-        surfaceHolder = getHolder();
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
     }
 
+
     public boolean onTouchEvent(MotionEvent motionEvent){
-        if (motionEvent.getAction()==MotionEvent.ACTION_DOWN) {
-            canvas = surfaceHolder.lockCanvas();
-            Paint paint = new Paint();
-            canvas.drawCircle(motionEvent.getX(), motionEvent.getY(), startRadius, paint); // v draw jjjoopa
-            return true;
-        }
-        return false;
+        drawThread.setParams(motionEvent.getX(), motionEvent.getY(), 0);
+        return super.onTouchEvent(motionEvent);
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
+        drawThread = new DrawThread(getContext(),getHolder());
+        drawThread.start();
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
+        drawThread.requestStop();
+        boolean retry = true;
+        while (retry) {
+            try {
+                drawThread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+                //
+            }
+        }
     }
 
 }
 
 
-class DrawThread extends Thread{
+class DrawThread extends Thread {
 
-    public void run(){
+    private SurfaceHolder surfaceHolder;
+    private volatile boolean running = true;
+    private float x = 0, y = 0, r = 0;
+    private Paint paint;
 
+    DrawThread(Context context, SurfaceHolder surfaceHolder) {
+        this.surfaceHolder = surfaceHolder;
+        paint = new Paint();
+    }
+
+    void setParams(float x, float y, float r){
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        paint.setColor(Color.YELLOW);
+    }
+
+    public void requestStop() {
+        running = false;
+    }
+
+    @Override
+    public void run() {
+        paint.setColor(Color.BLUE);
+
+        while (running) {
+            Canvas canvas = surfaceHolder.lockCanvas();
+            if (canvas != null) {
+                try {
+                    canvas.drawColor(Color.BLUE);
+                    canvas.drawCircle(x, y, r, paint);
+                } finally {
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+                r += 5;
+
+                try {
+                    Thread.sleep(17);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
